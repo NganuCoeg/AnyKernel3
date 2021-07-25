3e729f8b11e211b52ffc8c5ea974ea00f7a27eb5
NganuCoeg <starterpackmeh@gmail.com>
2021-07-25 13:55:18 +0000

    anykernel.sh: Rebrand

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=StrayDogs by MataneTjok
+kernel.string=ProjectDedMen by MataneTjok
 do.devicecheck=0
 do.modules=0
 do.systemless=1
